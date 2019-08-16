96934705253a376ab45690a3a45a7d19b30f1747
Patroklos <patstefanou@gmail.com>
2019-08-16 20:47:22 +0300

    Navbar Js Work

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,6 +2,25 @@
  * navigation bar
 */
 
-$( document ).ready(function() {
+$(document).ready(function() {
+  var primaryNavbarBar = $('#primary-navbar');
+  var secondaryNavbar = $('#secondary-navbar');
 
-} 
+  // if the user scrolls down the web page --> navbar changes
+  $(body).scroll(function() {
+    // changing thresholds/variables
+    var this.change = navbar.offsetTop;
+    var this.changeNavbar = navbar.pageYOffset;
+
+    // swap navbar if different
+    if (this.changeNavbar >= this.change) {
+      primaryNavbarBar.addClass('.invisible-navbar');
+      secondaryNavbar.removeClass('.invisible-navbar');
+    } else {
+      secondaryNavbar.addClass('.invisible-navbar');
+    }
+
+  })
+
+
+}
---
+++ javascript/resumeButton.js
@@ -0,0 +1,18 @@
+/*
+ * resume button
+*/
+
+var ResumeButton = function() {
+  this.id = 'resume';
+
+  var that = Object.create(ResumeButton.prototype);
+
+  that.button = function() {
+      var button = document.createElement("button");
+
+      // general resume button
+      
+
+  }
+
+}

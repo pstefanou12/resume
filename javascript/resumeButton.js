c86bd7d3ef4650397e5a1e95659842b685f25790
Patroklos <patstefanou@gmail.com>
2019-08-18 20:03:55 +0300

    Work on Header Image

--- javascript/resumeButton.js
+++ javascript/resumeButton.js
@@ -9,10 +9,16 @@ var ResumeButton = function() {
 
   that.button = function() {
       var button = document.createElement("button");
+      $(button).id = id;
 
       // general resume button
-      
+      var resumeLink = document.createElement("a");
+      $(resumeLink).attr('href', "../stefanouResume.pdf");
+      $(resumeLink).attr('target', '_blank');
 
-  }
+      $(button).append(resumeLink);
+  };
 
+  Object.freeze(that); // object now immutable
+  return that;
 }

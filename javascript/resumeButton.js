707e032d1ee1225893ef6b8e192a46083c45a2e1
Patroklos <patstefanou@gmail.com>
2019-08-23 07:19:34 -0400

    Buttons and Pictures

--- javascript/resumeButton.js
+++ javascript/resumeButton.js
@@ -2,23 +2,7 @@
  * resume button
 */
 
-var ResumeButton = function() {
-  this.id = 'resume';
-
-  var that = Object.create(ResumeButton.prototype);
-
-  that.button = function() {
-      var button = document.createElement("button");
-      $(button).id = id;
-
-      // general resume button
-      var resumeLink = document.createElement("a");
-      $(resumeLink).attr('href', "../stefanouResume.pdf");
-      $(resumeLink).attr('target', '_blank');
-
-      $(button).append(resumeLink);
-  };
-
-  Object.freeze(that); // object now immutable
-  return that;
-}
+// class: css class for specific button instance
+$(document).ready(function() {
+  
+});
